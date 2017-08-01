19278ba3f493b0cc44d4e1a6320ebb5afb636bd8
minbo <hemin_it@163.com>
2017-08-01 17:07:52 +0800

    update

--- aylson-manage/src/main/java/com/aylson/dc/cfdb/service/impl/UserTasklistServiceImpl.java
+++ aylson-manage/src/main/java/com/aylson/dc/cfdb/service/impl/UserTasklistServiceImpl.java
@@ -11,12 +11,14 @@ import com.aylson.core.frame.domain.ResultCode;
 import com.aylson.core.frame.service.impl.BaseServiceImpl;
 import com.aylson.dc.cfdb.dao.ImUsersDao;
 import com.aylson.dc.cfdb.dao.IncomeHisDao;
+import com.aylson.dc.cfdb.dao.TasklistDao;
 import com.aylson.dc.cfdb.dao.UserTasklistDao;
 import com.aylson.dc.cfdb.po.IncomeHis;
 import com.aylson.dc.cfdb.po.UserTasklist;
 import com.aylson.dc.cfdb.search.UserTasklistSearch;
 import com.aylson.dc.cfdb.service.UserTasklistService;
 import com.aylson.dc.cfdb.vo.ImUsersVo;
+import com.aylson.dc.cfdb.vo.TasklistVo;
 import com.aylson.dc.cfdb.vo.UserTasklistVo;
 import com.aylson.dc.sys.common.SessionInfo;
 import com.aylson.utils.DateUtil2;
@@ -35,6 +37,9 @@ public class UserTasklistServiceImpl  extends BaseServiceImpl<UserTasklist, User
 	
 	@Autowired
 	private IncomeHisDao incomeHisDao;
+	
+	@Autowired
+	private TasklistDao tasklistDao;
 
 	@Override
 	protected BaseDao<UserTasklist, UserTasklistSearch> getBaseDao() {
@@ -79,6 +84,8 @@ public class UserTasklistServiceImpl  extends BaseServiceImpl<UserTasklist, User
 				}else {
 					actionFlag = 3;	//未扣钱，直接审批失败，不记录收益
 				}
+				//审批失败时，需把相应的任务数加1
+				this.increaseTaskNum(userTasklistVo.getTaskId(), request, cTime);
 			}
 			
 			boolean flag1 = this.userTasklistDao.updateById(userTasklistVo);	//更新任务审批状态
@@ -90,31 +97,59 @@ public class UserTasklistServiceImpl  extends BaseServiceImpl<UserTasklist, User
 			}
 			
 			if(actionFlag != 3) {
-				SessionInfo sessionInfo = (SessionInfo)request.getSession().getAttribute("sessionInfo");
 				//3. 记录用户收益记录情况
-				IncomeHis incomeHis = new IncomeHis();
-				incomeHis.setId(UUIDUtils.create());
-				incomeHis.setPhoneId(userTasklistVo.getPhoneId());
-				incomeHis.setTaskId(userTasklistVo.getTaskId());
-				incomeHis.setLogoUrl(userTasklistVo.getLogoUrl());
-				incomeHis.setTaskName(userTasklistVo.getTaskName());
-				incomeHis.setIncomeTime(cTime);
-				incomeHis.setIncome(userTasklistVo.getIncome());
-				incomeHis.setCreateDate(cTime);
-				incomeHis.setCreatedBy(sessionInfo.getUser().getUserName() + "/" + sessionInfo.getUser().getRoleName());
-				incomeHis.setUpdateDate(cTime);
-				incomeHis.setFlag(actionFlag);	//1=加钱；2=扣钱
-				incomeHis.setChannel(1);			//1=后台系统广告；2=SDK平台广告
-				boolean flag3 = this.incomeHisDao.insert(incomeHis);				//记录用户收益记录情况
-				if(!flag3) {
-					logger.warn("记录用户收益记录失败，请查核。phoneId=" + userTasklistVo.getPhoneId() 
-							+ ", taskId=" + userTasklistVo.getTaskId());
-				}
+				this.recordIncomeHis(userTasklistVo, request, actionFlag, cTime);;
 			}
 		}catch(Exception e){
 			logger.error(e.getMessage(), e);
 			result.setError(ResultCode.CODE_STATE_500, e.getMessage());
 		}
 		return result;
+	}
+	
+	/**
+	 * 审批失败时，需把相应的任务数加1
+	 * @param taskId
+	 */
+	private void increaseTaskNum(String taskId, HttpServletRequest request, String cTime) {
+		SessionInfo sessionInfo = (SessionInfo)request.getSession().getAttribute("sessionInfo");
+		//审批失败时，需把任务数加1
+		TasklistVo taskListVo =  this.tasklistDao.selectById(taskId);
+		taskListVo.setAmount(taskListVo.getAmount()+1);	//任务当前剩余数量加1
+		taskListVo.setUpdateDate(cTime);
+		taskListVo.setUpdatedBy(sessionInfo.getUser().getUserName() + "/" + sessionInfo.getUser().getRoleName());
+		boolean flag = this.tasklistDao.updateById(taskListVo);
+		if (!flag) {
+			logger.warn("审批失败时，任务数回退加1失败，请查核。taskId=" + taskId);
+		}
+	}
+	
+	/**
+	 * 记录用户收益记录情况
+	 * @param userTasklistVo 用户任务
+	 * @param request 当前请求
+	 * @param actionFlag 操作标识位，1=加钱，2=扣钱
+	 * @param cTime 当前时间
+	 */
+	private void recordIncomeHis(UserTasklistVo userTasklistVo, HttpServletRequest request, int actionFlag, String cTime) {
+		SessionInfo sessionInfo = (SessionInfo)request.getSession().getAttribute("sessionInfo");
+		IncomeHis incomeHis = new IncomeHis();
+		incomeHis.setId(UUIDUtils.create());
+		incomeHis.setPhoneId(userTasklistVo.getPhoneId());
+		incomeHis.setTaskId(userTasklistVo.getTaskId());
+		incomeHis.setLogoUrl(userTasklistVo.getLogoUrl());
+		incomeHis.setTaskName(userTasklistVo.getTaskName());
+		incomeHis.setIncomeTime(cTime);
+		incomeHis.setIncome(userTasklistVo.getIncome());
+		incomeHis.setCreateDate(cTime);
+		incomeHis.setCreatedBy(sessionInfo.getUser().getUserName() + "/" + sessionInfo.getUser().getRoleName());
+		incomeHis.setUpdateDate(cTime);
+		incomeHis.setFlag(actionFlag);	//1=加钱；2=扣钱
+		incomeHis.setChannel(1);			//1=后台系统广告；2=SDK平台广告
+		boolean flag = this.incomeHisDao.insert(incomeHis);				//记录用户收益记录情况
+		if(!flag) {
+			logger.warn("记录用户收益记录失败，请查核。phoneId=" + userTasklistVo.getPhoneId() 
+					+ ", taskId=" + userTasklistVo.getTaskId());
+		}
 	}
 }
